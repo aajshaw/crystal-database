02d0b8e659ca3f5725f21dedd62ea6de35ff45b8
Anthony Shaw <aajshaw@adsitsol.co.uk>
2018-05-16 18:02:53 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 8082
+
+const passport = require('passport');
+const flash = require('connect-flash');
+
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+
+const config = require('./config/config');
+//const db = require('./db/db')();
+
+//require('./config/passport')(passport, db); // pass passport for configuration
+
+// Set up express application
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set('view engine', 'ejs');
+
+// Set up passport
+app.use(session({
+  secret: config.get('session_secret'),
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+require('./app/routes.js')(app, passport, db);
+
+app.listen(port);
+console.log("Listening on port " + port);
